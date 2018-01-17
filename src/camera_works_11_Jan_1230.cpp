a04a580dbdb8d3cc5472af8b28cf19d2869d739d
Amrutavarsh S Kinagi <askinagi@ust.hk>
2018-01-18 04:02:47 +0800

    turning algorithm updated

--- src/camera_works_11_Jan_1230.cpp
+++ src/camera_works_11_Jan_1230.cpp
@@ -8,6 +8,7 @@
 
 #include <cassert>
 #include <cstring>
+#include <functional>
 #include <libbase/k60/mcg.h>
 #include <libsc/system.h>
 #include <libsc/led.h>
@@ -22,22 +23,24 @@
 #include <vector>
 #include <math.h>
 #include <libsc/alternate_motor.h>
+#include <libbase/k60/adc.h>
+#include <libsc/joystick.h>
+#include <libsc/dir_encoder.h>
 #include "coord.h"
 
-namespace libbase
-{
-    namespace k60
-    {
+#define PI 3.14159265
 
-        Mcg::Config Mcg::GetMcgConfig()
-        {
-            Mcg::Config config;
-            config.external_oscillator_khz = 50000;
-            config.core_clock_khz = 150000;
-            return config;
-        }
+namespace libbase {
+namespace k60 {
 
-    }
+Mcg::Config Mcg::GetMcgConfig() {
+	Mcg::Config config;
+	config.external_oscillator_khz = 50000;
+	config.core_clock_khz = 150000;
+	return config;
+}
+
+}
 }
 
 using libsc::System;
@@ -49,119 +52,239 @@ using namespace std;
 #define Height 60
 bool camptr[Height][Width];
 
-void extract_cam (const Byte* camBuffer) {
-    Uint pos = 0;
-    int bit_pos = 8;
-    for(Uint i=0; i < Height; i++){
-        for(Uint j = 0;j<Width; j++){
-            if (--bit_pos < 0) // Update after 8 bits are read
-            {
-                bit_pos = 7;// to track which position in a branch of Byte(Totally 8) is being read now.
-                ++pos;// to track which position in Byte array is being read now.
-            }
-            camptr[i][j] = GET_BIT(camBuffer[pos], bit_pos);
-        }
-    }
+inline bool ret_cam_bit(int x, int y, const Byte* camBuffer) {
+	return ((camBuffer[y * 10 + x / 8] >> (7 - (x % 8))) & 1);
 }
+//
+//void extract_cam (const Byte* camBuffer) {
+//    Uint pos = 0;
+//    int bit_pos = 8;
+//    for(Uint i=0; i < Height; i++){
+//        for(Uint j = 0;j<Width; j++){
+//            if (--bit_pos < 0) // Update after 8 bits are read
+//            {
+//                bit_pos = 7;// to track which position in a branch of Byte(Totally 8) is being read now.
+//                ++pos;// to track which position in Byte array is being read now.
+//            }
+//            camptr[i][j] = GET_BIT(camBuffer[pos], bit_pos);
+//        }
+//    }
+//}
 
 St7735r* lcdP;
 
-St7735r::Config getLcdConfig()
-{
-    St7735r::Config config;
-    config.orientation = 2;
-    return config;
+St7735r::Config getLcdConfig() {
+	St7735r::Config config;
+	config.orientation = 2;
+	return config;
 }
 
-k60::Ov7725::Config getCameraConfig()
-{
-    k60::Ov7725::Config config;
-    config.id = 0;
-    config.w = Width;
-    config.h = Height;
-    config.fps = k60::Ov7725Configurator::Config::Fps::kHigh;
-    return config;
+LcdTypewriter::Config GetWriterConfig(St7735r *temp_lcd) {
+	LcdTypewriter::Config config;
+	config.lcd = temp_lcd;
+	config.bg_color = 0;
+	config.text_color = 0xFFFF;
+	config.is_text_wrap = false;
+	return config;
 }
 
-FutabaS3010::Config getServoConfig()
-{
-    FutabaS3010::Config config;
-    config.id = 0;
-    return config;
+k60::Ov7725::Config getCameraConfig() {
+	k60::Ov7725::Config config;
+	config.id = 0;
+	config.w = Width;
+	config.h = Height;
+	config.fps = k60::Ov7725Configurator::Config::Fps::kHigh;
+	return config;
 }
 
-AlternateMotor::Config getMotorConfig()
-{
-    AlternateMotor::Config config;
-    config.id = 0;
-    return config;
+FutabaS3010::Config getServoConfig() {
+	FutabaS3010::Config config;
+	config.id = 0;
+	return config;
 }
+
+AlternateMotor::Config getMotorConfig() {
+	AlternateMotor::Config config;
+	config.id = 0;
+	return config;
+}
+
+Adc::Config getMagSensorConfig(Adc::Name adc_name) {
+	Adc::Config config;
+	config.adc = adc_name;
+	config.speed = Adc::Config::SpeedMode::kFast;
+	config.is_continuous_mode = true;
+	config.avg_pass = Adc::Config::AveragePass::k32;
+	return config;
+}
+
+Joystick::Config getJoystickConfig(Joystick::Listener isr) {
+	Joystick::Config config;
+	config.id = 0;
+	config.is_active_low = true;
+	config.dispatcher = isr;
+	return config;
+}
+
+Encoder::Config getEncoderConfig(){
+	Encoder::Config config;
+	config.id = 0;
+	return config;
+}
+
 /*
-void Print2D(){
-	for (int y=0; y<Height; y++){
-		for (int x=0; x<Width; x++){
-			lcdP->SetRegion(Lcd::Rect(x, y, 1, 1));
-			if (!camptr[y][x]){
-				lcdP->FillColor(0xFFFF);
-			} else {
-				lcdP->FillColor(0x0000);
+ void Print2D(){
+ for (int y=0; y<Height; y++){
+ for (int x=0; x<Width; x++){
+ lcdP->SetRegion(Lcd::Rect(x, y, 1, 1));
+ if (!camptr[y][x]){
+ lcdP->FillColor(0xFFFF);
+ } else {
+ lcdP->FillColor(0x0000);
+ }
+ }
+ }
+
+ }
+ */
+int main(void) {
+	System::Init();
+
+	k60::Ov7725 camera(getCameraConfig());
+	camera.Start();
+
+	St7735r lcd(getLcdConfig());
+	lcdP = &lcd;
+
+	LcdTypewriter writer(GetWriterConfig(&lcd));
+
+	FutabaS3010 Servo(getServoConfig());
+
+	AlternateMotor Motor(getMotorConfig());
+	Motor.SetPower(150);
+	Motor.SetClockwise(false);
+
+	Servo.SetDegree(90 * 10);
+
+	Adc MagSensor1(getMagSensorConfig(Adc::Name::kAdc0Ad8));
+	Adc MagSensor2(getMagSensorConfig(Adc::Name::kAdc0Ad9));
+	Adc MagSensor3(getMagSensorConfig(Adc::Name::kAdc1Ad6B));
+	Adc MagSensor4(getMagSensorConfig(Adc::Name::kAdc1Ad7B));
+
+	int32_t ticks = System::Time();
+//    int32_t brake_ticks = System::Time();
+	int pre_temp = 0;
+	int temp = 0;
+	float derivative;
+	float Kp = 1;
+	float Kd = 1;
+	bool Motor_start = true;
+
+//	Joystick::Config t = getJoystickConfig(
+//			[ ](const uint8_t id, const Joystick::State state) {
+//				if(state == Joystick::State::kRight)
+//				Kp += 0.02;
+//				else if(state == Joystick::State::kLeft)
+//				Kp -= 0.02;
+//				else if(state == Joystick::State::kUp)
+//				Kd += 0.02;
+//				else if(state == Joystick::State::kDown)
+//				Kd-=0.02;
+//			});
+
+    Joystick js(
+    		getJoystickConfig(
+    				[&Kp,&Kd](const uint8_t id, const Joystick::State state){
+    		if(state == Joystick::State::kLeft)
+    			Kp += 0.02;
+    		else if(state == Joystick::State::kRight)
+    			Kp -= 0.02;
+    		else if(state == Joystick::State::kDown)
+    			Kd += 0.02;
+    		else if(state == Joystick::State::kUp)
+    			Kd-=0.02;
+    }
+    				)
+	);
+
+	while (true) {
+		while (ticks != libsc::System::Time()) {
+			ticks = libsc::System::Time();
+			if (ticks % 10 == 0) {
+				ticks = System::Time();
+//				char c[15];
+				const Byte* camBuffer = camera.LockBuffer();
+//                extract_cam(camBuffer);
+////				Print2D();
+                lcdP->SetRegion(Lcd::Rect(0, 0, Width, Height));
+				lcdP->FillBits(0x0000, 0xFFFF, camBuffer, Width * Height);
+				camera.UnlockBuffer();
+				int l_count = 0;
+				int r_count = 0;
+				int l_correct = 0;
+				int r_correct = 0;
+				int l_small = 0;
+				int r_small = 0;
+				for (int j = 51; j < 55; j++) {
+					int l_counter = 0;
+					int r_counter = 0;
+					for (int i = 39; i > 0 && ret_cam_bit(i, j, camBuffer) != 1;
+							i--)
+						l_counter++;
+					for (int k = 41;
+							k < 80 && ret_cam_bit(k, j, camBuffer) != 1; k++)
+						r_counter++;
+					l_count += l_counter;
+					r_count += r_counter;
+//                		lcdP->SetRegion(Lcd::Rect(40-l_counter-2,j-2,5,5));
+//                		lcdP->FillColor(Lcd::kGreen);
+//                		lcdP->SetRegion(Lcd::Rect(40+r_counter-2,j-2,5,5));
+//                		lcdP->FillColor(Lcd::kCyan);
+					if (l_counter > 37)
+						l_correct++;
+					if (r_counter > 37)
+						r_correct++;
+					if (l_counter < 4)
+						l_small++;
+					if (r_counter < 4)
+						r_small++;
+				}
+				temp = ((r_count - l_count) * 100) / (r_count + l_count);
+				if ((l_correct && r_correct) || l_small || r_small)
+					temp = pre_temp;
+				derivative = (-temp + pre_temp) / 10;
+				Servo.SetDegree(900 - (0.87 * (-temp) + 0.85 * derivative) * 16);
+//                if(((0.9*(-temp)+0.8*derivative)*16>45 || (0.9*(-temp)+0.8*derivative)*16<-45) && System::Time() - brake_ticks >= 2000){
+//                		brake_ticks = System::Time();
+//                		Motor.SetPower(20);
+//                	    Motor.SetClockwise(true);
+//                	    System::DelayMs(10);
+//                	    Motor.SetPower(200);
+//                	    Motor.SetClockwise(false);
+//                }
+////
+//                lcdP->SetRegion(Lcd::Rect(0,62,120,15));
+//                sprintf(c,"temp :%d",temp);
+//                writer.WriteBuffer(c,15);
+//                lcdP->SetRegion(Lcd::Rect(0,79,120,15));
+//				sprintf(c,"pre_temp :%d",pre_temp);
+//				writer.WriteBuffer(c,15);
+//				lcdP->SetRegion(Lcd::Rect(0,96,120,15));
+//				sprintf(c,"derivative :%d",derivative);
+//				writer.WriteBuffer(c,15);
+//				lcdP->SetRegion(Lcd::Rect(0,113,120,15));
+//				sprintf(c,"turn :%f",(Kp * (-temp) + Kd * derivative) * 16.2);
+//				writer.WriteBuffer(c,15);
+//				lcdP->SetRegion(Lcd::Rect(0,130, 50, 15));
+//				sprintf(c, "Kp: %f", Kp);
+//				writer.WriteBuffer(c, 15);
+//				lcdP->SetRegion(Lcd::Rect(55,130, 50, 15));
+//				sprintf(c, "Kd: %f", Kd);
+//				writer.WriteBuffer(c, 15);
+				pre_temp = temp;
 			}
 		}
 	}
 
-}
-*/
-int main(void)
-{
-    System::Init();
-
-    k60::Ov7725 camera(getCameraConfig());
-    camera.Start();
-
-    St7735r lcd(getLcdConfig());
-    lcdP = &lcd;
-
-    FutabaS3010 Servo(getServoConfig());
-
-    AlternateMotor Motor(getMotorConfig());
-
-    Motor.SetPower(100);
-    Motor.SetClockwise(false);
-
-    Servo.SetDegree(90*10);
-
-    int32_t ticks = System::Time();
-    while (true){
-        while(ticks!=libsc::System::Time()){
-            ticks=libsc::System::Time();
-            if(ticks%10==0){
-                ticks = System::Time();
-                const Byte* camBuffer = camera.LockBuffer();
-                extract_cam(camBuffer);
-//				Print2D();
-                lcdP->SetRegion(Lcd::Rect(0, 0, Width, Height));
-                lcdP->FillBits(0x0000, 0xFFFF, camBuffer, Width * Height);
-                camera.UnlockBuffer();
-                int middle_coor = 40;
-                int right_coor = 0;
-                int left_coor = 0;
-                for(int i=50;i<55;i++){
-                    for(int j=40;j<80;j++){
-                        if(camptr[i][j] == 1) {right_coor += j;break;}
-                        else if (j == 79) right_coor+=j;
-                    }
-                }
-                right_coor /= 5;
-                for(int i=50;i<55;i++){
-                    for(int j=40;j>0;j--)
-                        if(camptr[i][j] == 1) {left_coor += j;break;}
-                }
-                left_coor /= 5;
-                int temp_coor = (left_coor + right_coor)/2;
-                Servo.SetDegree((90+temp_coor - 40)*10);
-            }
-        }
-    }
-
-    return 0;
+	return 0;
 }
