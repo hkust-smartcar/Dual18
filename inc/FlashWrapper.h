62e7020eaa1609560c54f3bca02e31e1f71c4ffc
josephyim224 <33196044+josephyim224@users.noreply.github.com>
2018-07-13 16:55:39 +0800

    Update FlashWrapper.h

--- inc/FlashWrapper.h
+++ inc/FlashWrapper.h
@@ -9,6 +9,16 @@
 #define INC_FLASHWRAPPER_H_
 
 #include "libbase/k60/flash.h"
+#include <stdlib.h>
+
+/*
+ * Flash
+ * 0: float size
+ * 1: int size
+ * 2+: data
+ */
+
+using libbase::k60::Flash;
 
 class FlashWrapper {
 public:
@@ -17,51 +27,72 @@ public:
 
 	}
 
-	bool readFloat(uint8_t floatNumber, std::vector<float> &f_, uint8_t intNumber, std::vector<float> &i_) {
+	bool read(std::vector<float> &f_, std::vector<int> &i_) {
 		f_.clear();
 		i_.clear();
 
-		Byte * floatBytes;
-		libbase::k60::Flash::FlashStatus status = flash.Read(floatBytes, floatNumber * 4);
+		// get the head of the flash
+		Byte * Bytes = (Byte *) malloc(2);
+		Flash::FlashStatus status = flash.Read(Bytes, 2);
 
-		if (status == libbase::k60::Flash::FlashStatus::kSuccess) {
-			float tempf;
-			uint8_t t = 0;
-			for (; t < floatNumber; t += 4) {
-				memcpy(&tempf, (floatBytes + t), 4);
-				f_.emplace_back(tempf);
-			}
-			int tempi;
-			for (; t < intNumber; t += 4) {
-				memcpy(&tempi, (floatBytes + t), 4);
-				i_.emplace_back(tempi);
-			}
-			return true;
-		} else {
+		uint8_t floatNumber = Bytes[0];
+		uint8_t intNumber = Bytes[1];
+		uint8_t size = (floatNumber + intNumber) * 4 + 2;
+		free(Bytes);
+
+		if (status != Flash::FlashStatus::kSuccess) {
 			return false;
 		}
+
+		// get data
+		Bytes = (Byte *) malloc(size);
+		status = flash.Read(Bytes, size);
+
+		if (status != Flash::FlashStatus::kSuccess) {
+			free(Bytes);
+			return false;
+		}
+
+		uint8_t t = 2; // start from 2
+		for (uint8_t i = 0; i < floatNumber; t += 4) {
+			float tempf;
+			memcpy(&tempf, (Bytes + t), 4);
+			f_.emplace_back(tempf);
+		}
+		for (uint8_t i = 0; i < intNumber; t += 4) {
+			int tempi;
+			memcpy(&tempi, (Bytes + t), 4);
+			i_.emplace_back(tempi);
+		}
+
+		free(Bytes);
+		return true;
 	}
 
-	bool writeFloat(std::vector<float> &f_, std::vector<int> &i_) {
-		uint8_t floatNumber = f_.size();
-		Byte * floatBytes;
+	bool write(std::vector<float> &f_, std::vector<int> &i_) {
+		uint8_t size = (f_.size() + i_.size()) * 4;
+		Byte * Bytes = (Byte *) malloc(size + 2);
 
-		uint8_t i = 0;
+		Bytes[0] = f_.size();
+		Bytes[1] = i_.size();
+
+		uint8_t i = 2;
 		for (std::vector<float>::iterator it = f_.begin(); it != f_.end(); it++, i += 4) {
 			// only a rather odd fix towards the problem
 			// not elagant, admittedly
 			float tf = *it; // dereference the iterator here
-			memcpy((floatBytes + i), &tf, 4);
+			memcpy((Bytes + i), &tf, 4);
 		}
 		for (std::vector<int>::iterator it = i_.begin(); it != i_.end(); it++, i += 4) {
 			// only a rather odd fix towards the problem
 			// not elagant, admittedly
 			int ti = *it; // dereference the iterator here
-			memcpy((floatBytes + i), &ti, 4);
+			memcpy((Bytes + i), &ti, 4);
 		}
+		free(Bytes);
 
-		libbase::k60::Flash::FlashStatus status = flash.Write(floatBytes, floatNumber * 4);
-		return (status == libbase::k60::Flash::FlashStatus::kSuccess);
+		Flash::FlashStatus status = flash.Write(Bytes, size);
+		return (status == Flash::FlashStatus::kSuccess);
 	}
 private:
 	libbase::k60::Flash flash;
